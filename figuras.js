34cb041db8965c2273f5affde02c6600d30c5010
Andrea Pabuence <beepabuence@gmail.com>
2022-04-24 20:59:17 -0500

    Se alñade codigo del cuadrado

--- figuras.js
+++ figuras.js
@@ -1,3 +1,31 @@
-console.log("Holis");
-var variable = prompt("Escribe lo que quieras: ");
-console.log(variable);
+//Codigo del cuadrado
+
+console.group("Cuadrado")
+
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: "+ladoCuadrado+"cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado mide: "+perimetroCuadrado+"cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado mide: "+areaCuadrado+"cm^2");
+
+console.groupEnd();
+
+//Codigo del triangulo
+console.group("Triangulo")
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log("Los lados del triangulo miden: "
+            +ladoTriangulo1
+            +"cm, " 
+            +ladoTriangulo2
+            +"cm, "
+            +baseTriangulo
+            +"cm"
+);
+console.groupEnd();
